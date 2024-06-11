3b703f4ecc59bddb0a4c9aa2041b2054fba2f871
M <dkmoon0530@gmail.com>
2024-06-11 14:59:59 +0900

    fix allowed hosts

--- mini7_8_web/mini7_8_web/settings.py
+++ mini7_8_web/mini7_8_web/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-vqcgga^o3v)(n6#+*3i$)zzt3fyu=&dyg(gqzxae!i!(&g!^@=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
