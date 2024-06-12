c18f2e96793f955e1b4fa5a8ad6caeeaef5dfe57
Han Kyuhyun <khh5345@naver.com>
2024-06-12 11:29:27 +0900

    Front Login, Signup 1차
    Signed-off-by: Han Kyuhyun <khh5345@naver.com>

--- mini7_8_web/accounts/urls.py
+++ mini7_8_web/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('', views.home, name='home'),  # 메인 페이지 URL 패턴 추가
     path('signup/', views.signup, name='signup'),
--- mini7_8_web/mini7_8_web/settings.py
+++ mini7_8_web/mini7_8_web/settings.py
@@ -145,9 +145,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
 
 # Default primary key field type
